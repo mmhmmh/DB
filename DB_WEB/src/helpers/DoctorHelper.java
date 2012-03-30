8b6ed72bb87486690b8f6744e6dd98b9b502eeb7
Mark Huang <mmhmmh@gmail.com>
2012-03-30 14:45:29 -0400

    Added Doctor Helper

---
+++ DB_WEB/src/helpers/DoctorHelper.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author Mark
+ */
+public class DoctorHelper {
+    public static String getName(int doctorId) {
+        return "Blah";
+    }
+}
