c62f9302a357cac123d07827f3d422773b752ed5
Mark Huang <mmhmmh@gmail.com>
2012-03-30 20:20:10 -0400

    Fixed appointment add

--- DB_WEB/src/helpers/AppointmentHelper.java
+++ DB_WEB/src/helpers/AppointmentHelper.java
@@ -151,13 +151,13 @@ public class AppointmentHelper {
         try {
             connection = DB.ConnectToDatabase();
             PS = connection.prepareStatement(
-                    "INSERT INTO appointments (patient_id, doctor_id, staff_id, appointment_start, appointment_end) VALUES (?,?,?,?,?)");
+                    "INSERT INTO appointments (patient_id, doctor_id, staff_id, appointment_start, appointment_end, appointment_status) VALUES (?,?,?,?,?,?)");
             PS.setInt(1, appointment.getPatientId());
             PS.setInt(2, appointment.getDoctorId());
             PS.setInt(3, appointment.getStaffId());
             PS.setTimestamp(4, new Timestamp(appointment.getStartTime()));
             PS.setTimestamp(5, new Timestamp(appointment.getEndTime()));
-            
+            PS.setString(6,appointment.getAppiontmentStatus());
             PS.execute();
             success = true;
         } catch (Exception e) {
